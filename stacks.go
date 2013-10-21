7c80b0c146a02f2794517eb4e417b10bd40f2aa6
JT Olds <jt@spacemonkey.com>
2013-10-21 23:21:32 +0000

    space monkey internal commit export
    [katamari commit: 2f644766f29565fb247e5141c6f1105437f0fe69]

--- stacks.go
+++ stacks.go
@@ -1,20 +1,3 @@
 // Copyright (C) 2013 Space Monkey, Inc.
 
 package log
-
-import (
-    "flag"
-    "log"
-    "runtime"
-)
-
-var (
-    stack_size = flag.Int("log.stack_trace_max_byte_length", 4096,
-        "The max stack trace byte length to log")
-)
-
-func PrintWithStack(message string) {
-    buf := make([]byte, *stack_size)
-    buf = buf[:runtime.Stack(buf, false)]
-    log.Printf("%s\n%s", message, buf)
-}
