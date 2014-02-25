d5be2774a9039038defdde8d42942ef7b08bf44a
Jeff Wendling <jeff@spacemonkey.com>
2014-02-25 13:45:14 -0500

    space monkey internal commit export
    [katamari commit: 9bd04d1d78e85304589695c66e328d23128f509c]

--- stderr.go
+++ stderr.go
@@ -3,69 +3,69 @@
 package log
 
 import (
-    "flag"
-    "log"
-    "os"
-    "os/exec"
-    "syscall"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
 
-    "code.spacemonkey.com/go/errors"
+	"code.spacemonkey.com/go/errors"
 )
 
 var (
-    syslog_binary = flag.String("logging_subprocess", "/usr/bin/logger",
-        "process to run for stderr-captured logging")
-    OutputCaptureError = errors.New(nil, "output capture error")
+	syslog_binary = flag.String("logging_subprocess", "/usr/bin/logger",
+		"process to run for stderr-captured logging")
+	OutputCaptureError = errors.New(nil, "output capture error")
 )
 
 func CaptureOutputToFd(fd int) error {
-    err := syscall.Dup2(fd, syscall.Stdout)
-    if err != nil {
-        return err
-    }
-    err = syscall.Dup2(fd, syscall.Stderr)
-    if err != nil {
-        return err
-    }
-    return nil
+	err := syscall.Dup2(fd, syscall.Stdout)
+	if err != nil {
+		return err
+	}
+	err = syscall.Dup2(fd, syscall.Stderr)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func CaptureOutputToFile(path string) error {
-    fh, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
-    if err != nil {
-        return err
-    }
-    defer fh.Close()
-    return CaptureOutputToFd(int(fh.Fd()))
+	fh, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+	return CaptureOutputToFd(int(fh.Fd()))
 }
 
 func CaptureOutputToSyslog(tag string) error {
-    cmd := exec.Command(*syslog_binary, "-t", tag)
-    out, err := cmd.StdinPipe()
-    if err != nil {
-        return err
-    }
-    defer out.Close()
-    type fder interface {
-        Fd() uintptr
-    }
-    out_fder, ok := out.(fder)
-    if !ok {
-        return OutputCaptureError.New("unable to get underlying pipe")
-    }
-    err = CaptureOutputToFd(int(out_fder.Fd()))
-    if err != nil {
-        return err
-    }
-    err = cmd.Start()
-    if err != nil {
-        return err
-    }
-    go func() {
-        err := cmd.Wait()
-        if err != nil {
-            log.Printf("captured output process died! %s", err)
-        }
-    }()
-    return nil
+	cmd := exec.Command(*syslog_binary, "-t", tag)
+	out, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	type fder interface {
+		Fd() uintptr
+	}
+	out_fder, ok := out.(fder)
+	if !ok {
+		return OutputCaptureError.New("unable to get underlying pipe")
+	}
+	err = CaptureOutputToFd(int(out_fder.Fd()))
+	if err != nil {
+		return err
+	}
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	go func() {
+		err := cmd.Wait()
+		if err != nil {
+			log.Printf("captured output process died! %s", err)
+		}
+	}()
+	return nil
 }
